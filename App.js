feb1f07da6c52042a6eb3d405d335f977d502cc8
zhenyezhao <86699069+zhenyezhao@users.noreply.github.com>
2022-02-20 19:08:23 -0800

    Add files via upload

--- App.js
+++ App.js
@@ -4,10 +4,9 @@ import BottomTabNavigator from "./components/BottomTabNavigator";
 export default class App extends Component {
   render() {
 
-    //return <BottomTabNavigator>;
-    //<BottomTabNavigator />;
-    //return <BottomTabNavigator />;
-    //return <BottomTabNavigator> <BottomTabNavigator />;
+ 
+    return <BottomTabNavigator />;
+ 
     
   }
 }
